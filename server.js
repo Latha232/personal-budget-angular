158c4612e63453188679de34a8d76d65c3681d13
sasi <sasi@xyz.com>
2023-09-17 13:36:50 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,35 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.use('/', express.static('public'));
-
-const budget = {
-   myBudget: [
-   {
-      title: 'Eat out',
-      budget: 25
-   },
-   {
-      title: 'Rent',
-      budget: 375
-   }, 
-   {
-      title: 'Grocery',
-      budget: 110
-   },
-   ]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    // Read data from JSON file each time the /budget endpoint is hit
+    const budgetData = JSON.parse(fs.readFileSync('budget-data.json', 'utf8'));
+    res.json(budgetData);
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at https://localhost:${port}`);
 });
-
